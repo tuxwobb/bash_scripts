b31b34b318a12c34d09a2dd0b3c2dae79f85ba29
tuxwobb <tom.miklik@gmail.com>
2026-04-08 21:46:09 +0200

    nvim installation in toolbox_debian.sh script

--- toolbox_debian.sh
+++ toolbox_debian.sh
@@ -15,8 +15,8 @@ HOME_DIR="/home/${USER}"
 TOOLBOX_DIR="${HOME_DIR}/Downloads/toolbox"
 
 # Urls
+NEOVIM_APPIMAGE_URL='https://github.com/neovim/neovim/releases/download/v0.12.1/nvim-linux-arm64.appimage'
 YAZI_URL='https://github.com/sxyazi/yazi/releases/download/v26.1.22/yazi-aarch64-unknown-linux-gnu.deb'
-# YAZI_URL='https://github.com/sxyazi/yazi/releases/download/v26.1.22/yazi-x86_64-unknown-linux-gnu.deb'
 LAZYVIM_URL='https://github.com/LazyVim/starter'
 VIMPLUG_URL='https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 TPM_URL='https://github.com/tmux-plugins/tpm'
@@ -25,8 +25,10 @@ usage() {
   echo "Usage: $0 [-h] [-v] [-bylpt] [-a]"
   echo "  Script to install development environment on Debian"
   echo "    -h  man page"
-  echo "    -v  verbose output of installation commands"
+  echo "    -v  log all messages"
+  echo "    -b  install basic applications"
   echo "    -y  install yazi"
+  echo "    -n  install neovim"
   echo "    -l  install lazyvim"
   echo "    -p  install vim vim-plug"
   echo "    -t  install tmux tpm"
@@ -36,7 +38,10 @@ usage() {
 
 # Log function
 log() {
-    echo ">>> ${*}"
+  echo ">>> ${*}"
+  if [[ $VERBOSE == 'true' ]]; then
+    logger ">>> ${*}"
+  fi
 }
 
 # Test if the script is running under root account
@@ -47,60 +52,68 @@ test_root() {
   fi
 }
 
+create_toolbox() {
+  log "Creating $TOOLBOX_DIR directory."
+  if [[ -d $TOOLBOX_DIR ]]; then
+    log "$TOOLBOX_DIR already exist."
+  else
+    if ! mkdir -p $TOOLBOX_DIR &>/dev/null; then
+      log "Error while creating directory ${TOOLBOX_DIR}." >&2
+      exit 1
+    fi
+    log "Directory $TOOLBOX_DIR created succesfully."
+
+    log "Change owner of directory ${TOOLBOX_DIR}"
+    if ! chown -R ${USER}:${GROUP} $TOOLBOX_DIR &>/dev/null; then
+      log "Change owner of directory ${TOOLBOX_DIR} failed." >&2
+      exit 1
+    fi
+    log "Change owner of directory ${TOOLBOX_DIR} completed."
+  fi
+}
+
 # Basic applications installation function
 basic_install() {
-  log "Installing of basic applications: ${APPS[@]}"
-  if ! sudo apt-get install -yq "${APPS[@]}" ; then
+  log "Installing of basic applications: ${APPS[*]}"
+  if ! sudo apt-get install -yq "${APPS[@]}"; then
     log "Basic applications installation failed." >&2
     exit 1
   fi
-  log "Basic applications installation successfull."
+  log "Basic applications installation completed."
 }
 
 # Yazi installation
 yazi_install() {
 
-  log "Installing of Yazi dependencies: ${YAZI_DEP[@]}"
-  if ! sudo apt-get install -yq "${YAZI_DEP[@]}" ; then
+  log "Installing of Yazi dependencies: ${YAZI_DEP[*]}"
+  if ! sudo apt-get install -yq "${YAZI_DEP[@]}"; then
     log "Yazi dependencies installation failed." >&2
     exit 1
   fi
-  log "Yazi dependencies installation successfull."
+  log "Yazi dependencies installation completed."
 
-  log "Creating $TOOLBOX_DIR directory..."
-  if ! mkdir -p $TOOLBOX_DIR &>/dev/null; then
-    log "Error while creating directory ${TOOLBOX_DIR}." >&2
-    exit 1
-  fi
-  log "Directory $TOOLBOX_DIR created succesfully."
-
-  log "Change owner of directory ${TOOLBOX_DIR}"
-  if ! chown -R ${USER}:${GROUP} $TOOLBOX_DIR &>/dev/null; then
-    log "Change owner of directory ${TOOLBOX_DIR} failed." >&2
-    exit 1
-  fi
-  log "Change owner of directory ${TOOLBOX_DIR} successfull."
+  create_toolbox
 
   log "Downloading Yazi from ${YAZI_URL}"
   if ! wget -P ${TOOLBOX_DIR} ${YAZI_URL} &>/dev/null; then
     log "Download Yazi from ${YAZI_URL} failed." >&2
     exit 1
   fi
-  log "Downloading Yazi from ${YAZI_URL} successfull."
+  log "Downloading Yazi from ${YAZI_URL} completed."
 
   log "Installing Yazi package"
   if ! dpkg -i ${TOOLBOX_DIR}/yazi* &>/dev/null; then
     log "Installing Yazi package failed." >&2
     exit 1
   fi
-  log "Installing Yazi package successfull."
+  log "Installing Yazi package completed."
 
   log "Deleting of Yazi installation file."
   if ! rm ${TOOLBOX_DIR}/yazi* &>/dev/null; then
     log "Deleting of Yazi installation file failed." >&2
     exit 1
   fi
-  log "Deleting of Yazi installation file successfull."
+  log "Deleting of Yazi installation file completed."
 }
 
 # Tmux TPM installation function
@@ -110,14 +123,14 @@ tmux_tpm_install() {
     log "Downloading tpm from ${TPM_URL} failed." >&2
     exit 1
   fi
-  log "Downloading tpm from ${TPM_URL} successfull."
+  log "Downloading tpm from ${TPM_URL} completed."
 
   log "Change owner of ${HOME_DIR}/.tmux directory."
   if ! chown -R ${USER}:${GROUP} ${HOME_DIR}/.tmux &>/dev/null; then
     log "Change owner of ${HOME_DIR}/.tmux failed" >&2
     exit 1
   fi
-  log "Change owner of ${HOME_DIR}/.tmux directory successfull."
+  log "Change owner of ${HOME_DIR}/.tmux directory completed."
 }
 
 # Vimplug installation function
@@ -127,13 +140,38 @@ vimplug_install() {
     log "Downloading vimplug from ${VIMPLUG_URL} failed." >&2
     exit 1
   fi
-  log "Downloading vimplug from ${VIMPLUG_URL} successfull."
+  log "Downloading vimplug from ${VIMPLUG_URL} completed."
 
   log "Change owner of ${HOME_DIR}/.vim directory."
   if ! chown -R ${USER}:${GROUP} ${HOME_DIR}/.vim &>/dev/null; then
     log "Change owner of ${HOME_DIR}/.vim directory failed." >&2
   fi
-  log "Change owner of ${HOME_DIR}/.vim direcgory successfull."
+  log "Change owner of ${HOME_DIR}/.vim direcgory completed."
+}
+
+# neovim installation function
+neovim_install() {
+  create_toolbox
+
+  log "Downloading nevim appimage from $NEOVIM_APPIMAGE_URL"
+  if ! (cd $TOOLBOX_DIR && wget $NEOVIM_APPIMAGE_URL &>/dev/null); then
+    log "Downloading neovim appimage failed." >&2
+    exit 1
+  fi
+  log "Dowloading neovim appimage completed."
+
+  local NEOVIM_FILE=$(cd $TOOLBOX_DIR && ls | grep nvim | head -1)
+  if ! (cd $TOOLBOX_DIR && mv -f "$NEOVIM_FILE" /opt &>/dev/null); then
+    log "Moving neovim appimage into /opt directory failed."
+    exit 1
+  fi
+  log "Moving neovim appimage into /opt directory completed."
+
+  if ! (cd $TOOLBOX_DIR && ln -sfT /opt/"$NEOVIM_FILE" /usr/local/bin/nvim &>/dev/null); then
+    log "Creating of symlink to neovim appimage failed."
+    exit 1
+  fi
+  log "Creating of symlink neovim appimage completed."
 }
 
 # Lazyvim installation function
@@ -143,24 +181,24 @@ lazyvim_install() {
     log "Downloading of LazyVim from ${LAZYVIM_URL} failed." >&2
     exit 1
   fi
-  log "Downloading of LazyVim from ${LAZYVIM_URL} successfull."
+  log "Downloading of LazyVim from ${LAZYVIM_URL} completed."
 
   log "Change owner of ${HOME_DIR}/.config/nvim directory."
   if ! chown -R ${USER}:${GROUP} ${HOME_DIR}/.config/nvim &>/dev/null; then
     log "Change owner of ${HOME_DIR}/.config/nvim direcgory failed." >&2
     exit 1
   fi
-  log "Change owner of ${HOME_DIR}/.config/nvim directory successfull."
+  log "Change owner of ${HOME_DIR}/.config/nvim directory completed."
 
   log "Deleting of ${HOME_DIR}/.config/nvim/.git directory."
   if ! rm -rf ${HOME_DIR}/.config/nvim/.git &>/dev/null; then
     log "Deleting of ${HOME_DIR}/.config/nvim.git file failed." >&2
   fi
-  echo ">>> Deleting of ${HOME_DIR}/.config/nvim.git file successful."
+  echo ">>> Deleting of ${HOME_DIR}/.config/nvim.git file completed."
 }
 
 # Main app
-while getopts hvbylpta OPTION; do
+while getopts hvbynlpta OPTION; do
   case $OPTION in
   h)
     usage
@@ -173,6 +211,9 @@ while getopts hvbylpta OPTION; do
     ;;
   y)
     INSTALL_YAZI='true'
+    ;;
+  n)
+    INSTALL_NEOVIM='true'
     ;;
   l)
     INSTALL_LAZYGIT='true'
@@ -203,6 +244,10 @@ if [[ $INSTALL_YAZI == 'true' ]]; then
   yazi_install
 fi
 
+if [[ $INSTALL_NEOVIM ]]; then
+  neovim_install
+fi
+
 if [[ $INSTALL_LAZYGIT == 'true' ]]; then
   tmux_tpm_install
 fi
@@ -220,6 +265,7 @@ if [[ $INSTALL_ALL == 'true' ]]; then
   yazi_install
   tmux_tpm_install
   vimplug_install
+  neovim_install
   lazyvim_install
 fi
 
