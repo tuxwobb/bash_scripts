acb6aaceaba8fb060661b81454be3d2bdfbb6b67
tux_wobb <tom.miklik@gmail.com>
2026-04-09 15:40:36 +0200

    new script run-everywhere.sh

---
+++ run-everywhere.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -o pipefail
+
+SERVER_FILE='servers'
+
+# Usage function
+usage() {
+  echo "Usage: $0 [-f FILE] [-n] [-s] [-v] COMMAND"
+  echo "  Script to run command on remote machines, default list of servers in file servers"
+  echo "    -f FILE  custom file with list of servers"
+  echo "    -n       dry-run"
+  echo "    -s       run as sudo"
+  echo "    -v       verbose mode"
+  echo
+  exit 0
+}
+
+# Parsing arguments
+while getopts f:nsv OPTION; do
+  case "$OPTION" in
+  f)
+    SERVER_FILE=$OPTARG
+    ;;
+  n)
+    DRY_RUN='true'
+    ;;
+  s)
+    SUDO='true'
+    ;;
+  v)
+    VERBOSE='true'
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ $# -eq 0 ]]; then
+  usage
+fi
+
+if [[ ! -e $SERVER_FILE ]]; then
+  echo "File $SERVER_FILE does not exist."
+  exit 1
+fi
+
+# Main loop
+COMMAND="$*"
+
+while read -e LINE; do
+  # verbose output
+  if [[ $VERBOSE == 'true' ]]; then
+    echo ">>> ${LINE}"
+  fi
+
+  # dry-run
+  if [[ $DRY_RUN == 'true' ]]; then
+    echo "ssh $LINE $COMMAND"
+  else
+    ssh $LINE $COMMAND
+  fi
+
+done <"$SERVER_FILE"
+
+exit 0
